5af7d9b170e3f363e43f945f88d8f2623aacbdb3
Seunghyun Yu <yshyun123@gmail.com>
2018-06-06 14:20:49 +0900

    57. Axios로 Ajax 리퀘스트.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-// const API_KEY = 'YOUR_OPEN_WEATHER_MAP_API_KEY';
+import axios from 'axios';
+
 const API_KEY = config.MY_KEY;
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}

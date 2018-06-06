87405762d8d4786ae0978e16b0d7d37ac1d4fd8c
Seunghyun Yu <yshyun123@gmail.com>
2018-06-06 15:39:31 +0900

    60. 리듀서의 스테이트 돌연변이 피하기.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,14 @@
+import {FETCH_WEATHER} from '../actions/index';
+
 // fetch weather 액션을 다루는 리듀서
-export default function (state = null, action) {
+export default function (state = [], action) {
   // console.log('Action received', action);
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.push(action.payload.data);
+      return [ action.payload.data, ...state];
+  }
   return state;
 
 }

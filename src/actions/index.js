77c6e38621eb412f19858f9968074aa8ef2bd20f
Seunghyun Yu <yshyun123@gmail.com>
2018-06-06 15:29:39 +0900

    59. 리덕스-프로미스(Promise) 활용 2.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,8 +7,11 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
+  const request = axios.get(url); // 프로미스는 어떤 데이터를 가지지 않는다.
 
+  // console.log('Request:', request);
+
+  // 페이로드의 키로 프로미스를 넣는다!
   return {
     type: FETCH_WEATHER,
     payload: request
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,6 @@
+// fetch weather 액션을 다루는 리듀서
+export default function (state = null, action) {
+  // console.log('Action received', action);
+  return state;
+
+}

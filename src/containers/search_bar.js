e2ead1a30b233f64a62ad1665ec258772a842da4
Seunghyun Yu <yshyun123@gmail.com>
2018-06-06 15:05:33 +0900

    58. 리덕스-프로미스(Promise) 활용 1.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,14 @@
-
-
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
     this.state = { term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,9 +20,9 @@ export default class SearchBar extends Component {
     // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
 
     // We need to go and fetch weather data
-    
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
   }
-
 
   render() {
     return (
@@ -38,3 +40,17 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+/**
+ * 액션 생성자로서, 호출될 때마다 dispatch와 함께 액션이 반환된다.
+ * 이 액션이 미들웨어로 흘러가 리덕스 어플리케이션 안의 리듀서로 들어가는 것을 확인할 수 있다.
+ *
+ * @param dispatch
+ * @returns {{fetchWeather: fetchWeather}|ActionCreator<any>|ActionCreatorsMapObject<any>}
+ */
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+
+export default connect(null, mapDispatchToProps)(SearchBar);
